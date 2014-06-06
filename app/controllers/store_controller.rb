3b5a024452f5cf854296bce8461c91287e6db834
ztoolson <zach.toolson@gmail.com>
2014-06-06 09:13:29 -0600

    Simple cart implemented

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    increment_count # increment session variable
+  end
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails4Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
 
